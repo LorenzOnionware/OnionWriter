42bd421aa02e3ca20cf22463194daeacda3bae95
lorenz | onionware <77418440+neutralezwiebel@users.noreply.github.com>
2022-06-21 16:30:51 +0200

    code cleanuup mini

--- OnionWriter/MainPage.xaml.cs
+++ OnionWriter/MainPage.xaml.cs
@@ -1,46 +1,12 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Threading;
-using System.Drawing;
-using Windows.Storage.Pickers;
-using Windows.Storage.Provider;
-using Windows.Storage;
-using Windows.UI.Text;
-using Windows.UI.Popups;
-using Windows.UI.Xaml.Documents;
-using Windows.ApplicationModel.DataTransfer;
-using Windows.UI.Xaml.Media.Imaging;
-using Windows.Storage.Streams;
-using System.Xml.Linq;
-using System.Text;
-using System.Xml;
-using Windows.UI.WindowManagement;
-using Windows.UI.Xaml.Hosting;
 using Windows.ApplicationModel.Core;
 using Windows.UI.ViewManagement;
-using Windows.UI.Core;
-using Windows.UI.Xaml.Printing;
-using Windows.Graphics.Printing;
-using Microsoft.Toolkit.Uwp.Helpers;
-using Microsoft.Toolkit.Uwp.UI.Helpers;
-using Microsoft.Win32;
-using Windows.ApplicationModel.Activation;
 using Microsoft.UI.Xaml.Controls;
 
 namespace Test
@@ -48,8 +14,6 @@ namespace Test
 
     public partial class Page : Windows.UI.Xaml.Controls.Page
     {
-        public int Counter;
-
         public Page()
         {
             InitializeComponent();
@@ -77,7 +41,6 @@ namespace Test
 
         private async void TabView_TabCloseRequested(Microsoft.UI.Xaml.Controls.TabView sender, Microsoft.UI.Xaml.Controls.TabViewTabCloseRequestedEventArgs args)
         {
-
             var Frame = args.Tab.Content as Frame;
             var Page = Frame.Content as BlankPage2;
             bool isEmpty = Page.TxtboxIsEmpty();
@@ -118,7 +81,6 @@ namespace Test
 
         private void TabView_Loaded(object sender, RoutedEventArgs e)
         {
-            Counter ++;
             (sender as TabView).TabItems.Add(CreateNewTab(1));
         }
 
